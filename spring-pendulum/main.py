d100fad49603c80d1158cb9bfea0b7b013ff9573
JudeB38 <131780184+JudeB38@users.noreply.github.com>
2023-04-27 20:37:43 +0100

    Main Animation Update
    Implemented basic animation to main project, needs some tweaks, mainly:
    - video output format
    - real time playback??

--- spring-pendulum/main.py
+++ spring-pendulum/main.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as anm
 
 spring_const = 50
 mass = 4
 initial_length = 1
 mu = 1
 g = 9.81
+global gamma
 
 def get_ml2d(th, l):
     return mass*g*np.cos(th) - spring_const*(l - initial_length)
@@ -18,31 +20,42 @@ def get_ngamma(gamma):
 
 gamma = np.array([0.3, 2., 1., -1.])
 
-dt = 0.001
-max_t = 10
-t = 0
-A=[]
-L=[]
-P_x, P_y = [], []
-while t <= max_t:
-    print(f"{gamma[0]:.2}, {gamma[1]:.2}, {gamma[2]:.2}, {gamma[3]:.2}")
+dt = 0.01
+time_elapsed = 5
+
+fig = plt.figure()
+ax = plt.axes(xlim=(-1.5, 1.5), ylim=(-4, 1))
+ax.scatter([0], [0])
+line, = ax.plot([], [], lw=2)
+
+
+def init():
+    line.set_data([], [])
+    return line,
+
+x_l, y_l = [], []
+def animate(t):
+    global gamma
+    print(f"t={t*dt:.3}: {gamma[0]:.2}, {gamma[1]:.2}, {gamma[2]:.2}, {gamma[3]:.2}")
     gamma += dt*get_ngamma(gamma)
     
     
-    #A.append(gamma[0])
-    #L.append(gamma[2])
-    P_x.append(gamma[2]*np.sin(gamma[0]))
-    P_y.append(-1*gamma[2]*np.cos(gamma[0]))
-    t += dt
+    x = gamma[2]*np.sin(gamma[0])
+    y = -1*gamma[2]*np.cos(gamma[0])
+    x_l.append(x)
+    y_l.append(y)
+    line.set_data(x_l, y_l)
+
+    return line,
 
 
-plt.scatter([0], [0])
-plt.plot(P_x, P_y)
-plt.xlim(left=-1, right=1)
-plt.ylim(top=0.1, bottom=-5)
-#plt.plot(A)
-#plt.show()
-#plt.plot(L)
-plt.show()
+FRAMES = int(time_elapsed/dt)
+FPS = int(1/dt)
+INTERVAL = int(dt*1000)
+
+anim = anm.FuncAnimation(fig, animate, init_func=init, frames=FRAMES, interval=INTERVAL, blit=True)
+anim.save('animation.gif', fps = FPS)
+print("DONE")
+
 
 
