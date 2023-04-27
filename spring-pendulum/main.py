bd7df793e19afcbf99cde581e10e1b22390290a6
JudeB38 <131780184+JudeB38@users.noreply.github.com>
2023-04-27 23:09:28 +0100

    mp4 animations
    changed animations to mp4 video format, saving them in their own subfolder. Program now automatically encodes the initial conditions of the system into the mp4 title.

--- spring-pendulum/main.py
+++ spring-pendulum/main.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anm
+import matplotlib as mpl
+
+mpl.rcParams['animation.ffmpeg_path'] = r'C:\\Users\\joann\\Documents\\FFmpeg\\ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe'
 
 spring_const = 50
 mass = 4
 initial_length = 1
 mu = 1
 g = 9.81
+parameters = spring_const, mass, initial_length, mu, g
 global gamma
 
 def get_ml2d(th, l):
@@ -18,7 +22,25 @@ def get_mth2d(th, thd, l, ld):
 def get_ngamma(gamma):
     return np.array([gamma[1]/mass, get_mth2d(gamma[0], gamma[1]/mass, gamma[2], gamma[3]/mass), gamma[3]/mass, get_ml2d(gamma[0], gamma[2])])
 
-gamma = np.array([0.3, 2., 1., -1.])
+def encode(gam, param, time):
+    out = ""
+    for i in gam:
+        if i < 0:
+            out += str(i) + "_"
+        else:
+            out += str(i) + "_"
+    
+    out += "K" + str(param[0]) + "_"
+    out += "M" + str(param[1]) + "_"
+    out += "L" + str(param[2]) + "_"
+    out += "U" + str(param[3]) + "_"
+    out += "T" + str(time)
+    
+    return out
+
+
+gamma = np.array([0.3, 2., 1., 1.])
+initial_gamma = gamma
 
 dt = 0.01
 time_elapsed = 5
@@ -36,7 +58,7 @@ def init():
 x_l, y_l = [], []
 def animate(t):
     global gamma
-    print(f"t={t*dt:.3}: {gamma[0]:.2}, {gamma[1]:.2}, {gamma[2]:.2}, {gamma[3]:.2}")
+    #print(f"t={t*dt:.3}: {gamma[0]:.2}, {gamma[1]:.2}, {gamma[2]:.2}, {gamma[3]:.2}")
     gamma += dt*get_ngamma(gamma)
     
     
@@ -54,8 +76,13 @@ FPS = int(1/dt)
 INTERVAL = int(dt*1000)
 
 anim = anm.FuncAnimation(fig, animate, init_func=init, frames=FRAMES, interval=INTERVAL, blit=True)
-anim.save('animation.gif', fps = FPS)
-print("DONE")
+
+name = encode(initial_gamma, parameters, time_elapsed)
+
+f = f"spring-pendulum/animations/anim_{name}.mp4"
+writermp4 = anm.FFMpegWriter(fps=FPS) 
+anim.save(f, writer=writermp4)
+print(name)
 
 
 
